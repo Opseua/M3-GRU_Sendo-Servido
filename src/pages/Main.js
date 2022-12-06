85d689812815978c15d3aa1c72d542ad3829ad28
Gaby França <gabriellyfranca810@gmail.com>
2022-12-06 00:15:44 -0300

    criado esqueleto da pagina inicial

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import Footer from "../components/Footer/Footer";
+import NavBar from "../components/NavBar/NavBar";
+
+export default function Main() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Footer />
+    </div>
+  );
+}
