a54b5a77ceb85f4c8d9913cc39baea5293d8dcdf
LaraSharmon <laranayt14@gmail.com>
2022-12-07 19:49:55 -0300

    fiz os cards com as imagens

---
+++ home/src/Card (1).js
@@ -0,0 +1,70 @@
+
+
+
+function Card() {
+  return (
+    <div id="App" className="App">
+      <h1 id="Titulo" className="Titulo">DREAMS IMOBILIÁRIA</h1>
+   
+
+      <div class="row">
+        <div class="card green">
+            <h1>R$1.000,00/mês</h1>
+            <h3>Rua Conde de Bonfim, RJ</h3>
+            <h4>4 Quartos | 3 Banheiros | 2 Garagem</h4>
+            <h4>Falar com Orlando : (21) 99040-6500</h4>
+          <img class="image" src="https://imgbr.imovelwebcdn.com/avisos/2/29/72/81/99/28/720x532/3588585487.jpg" alt="money" />
+        </div>
+
+        <div class="card green">
+            <h1>R$209,00/mês</h1>
+            <h3>Rua Oswaldo Lussac, RJ</h3>
+            <h4>2 Quartos | 3 Banheiros | 1 Garagem</h4>
+            <h4>Falar com Mariana : (21) 97980-1221</h4>
+            <img class="image" src="https://system.soprojetos.com.br/files/1335/medium/projeto-de-casa-terrea-para-terrenos-de-10-metros-com-3-quartos-e-1-suite-cod-175-fachada-1.jpg?1576186538" alt="money" />
+        </div>
+
+        <div class="card green">
+            <h1>R$660,00/mês</h1>
+            <h3>Almirante Ary Parreiras, RJ</h3>
+            <h4>1 Quarto | 1 Banheiro | 1 Garagem</h4>
+            <h4>Falar com Lara : (21) 98356-6482</h4>
+        <img class="image" src="https://system.soprojetos.com.br/files/996/medium/modelo_de_casa__versao_do_cod_99-cod_136-foto-1.jpg?1491503841" alt="money_1"/>
+     </div>
+
+    <div class="card green">
+      
+        <h1>R$1.610,00/mês</h1>
+        <h3>Avenida Atlântica, RJ</h3>
+        <h4>3 Quartos | 2 Banheiros | 1 Garagem</h4>
+        <h4>Falar com Yasmin : (21) 97205-4101</h4>
+        <img class="image" src="https://system.soprojetos.com.br/files/1491/medium/PAD-COD43-FOTO-1-WEB.jpg?1659015535" alt="money"/>
+         <br></br>
+      </div>
+
+    <div class="card green">
+
+        <h1>R$1.750,00/mês</h1>
+        <h3>Rua Porto Amazonas, RJ</h3>
+        <h4>3 Salas | 2 Banheiros | 2 Garagens</h4>
+        <h4>Falar com Brian : (21) 99255-0986</h4>
+        <img class="image" src="https://system.soprojetos.com.br/files/615/medium/casa_popular_cod100_2quartos.jpg?1491510183" alt="money" />
+    </div>
+
+    <div class="card green">
+
+        <h1>R$1.200,00/mês</h1>
+        <h3>Rua Voluntários da Pátria, RJ</h3>
+        <h4>2 Salas | 2 Banheiros | 0 Garagem</h4>
+        <h4>Falar com Gabrielly : (21) 99483-5925</h4>
+        <img class="image" src="https://system.soprojetos.com.br/files/597/medium/primeira_obra_da_madrugada.jpg?1491504043" alt="money" />
+   
+      </div>
+</div>     
+
+    </div >
+  );
+
+}
+
+export default Card;
--- home/src/index.js
+++ home/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import './style_card.css';
+import Card from './Card';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +14,22 @@ root.render(
   </React.StrictMode>
 );
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Card />
+  </React.StrictMode>
+);
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
+
