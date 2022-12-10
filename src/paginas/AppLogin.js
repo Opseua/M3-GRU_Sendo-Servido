14b75a543f596ef931adce7efb2c465aabd1b5b6
MarianaFigueiredoI <113534989+MarianaFigueiredoI@users.noreply.github.com>
2022-12-10 01:12:45 -0300

    Validação de formulário da página de login adicionada

--- src/paginas/AppLogin.js
+++ src/paginas/AppLogin.js
@@ -6,12 +6,34 @@ function AppLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [emailErr, setEmailErr] = useState(false);
+  const [passwordErr, setPasswordErr] = useState(false);
+
+  const validarEmail = new RegExp("^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$");
+  const validarSenha = new RegExp("^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$");
+
+  const validate = () => {
+      if(!validarEmail.test(email)) {
+          setEmailErr(true);
+      } else {
+          setEmailErr(false);
+      }
+
+      if(!validarSenha.test(password)) {
+          setPasswordErr(true);
+      } else {
+          setPasswordErr(false);
+      }
+  }
+  
+  console.log({email, password});
+
   return (
     <div className="container">
       <div className="container-login">
         <div className="wrap-login">
           <form className="login-form">
-            <span className="login-form-title"> Bem vindo </span>
+            <span className="login-form-title"> Bem-vindo! </span>
 
             <span className="login-form-title">
             </span>
@@ -23,7 +45,8 @@ function AppLogin() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-              <span className="focus-input" data-placeholder="Email"></span>
+              <span className="focus-input" data-placeholder="E-mail"></span>
+              {emailErr && <p>Por favor, digite um e-mail válido.</p>}
             </div>
 
             <div className="wrap-input">
@@ -31,13 +54,14 @@ function AppLogin() {
                 className={password !== "" ? "has-val input" : "input"}
                 type="password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setPassword(e.target.value)} required
               />
-              <span className="focus-input" data-placeholder="Password"></span>
+              <span className="focus-input" data-placeholder="Senha"></span>
+              {passwordErr && <p>Por favor, digite uma senha mais segura.</p>}
             </div>
 
             <div className="container-login-form-btn">
-              <button className="login-form-btn">Login</button>
+              <button onClick={validate} className="login-form-btn">Login</button>
             </div>
 
             <div className="text-center">
