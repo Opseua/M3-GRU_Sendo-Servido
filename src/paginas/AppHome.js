d4b0a19cf3aaf9acd35c5897002c6ddcd55ac406
MarianaFigueiredoI <113534989+MarianaFigueiredoI@users.noreply.github.com>
2022-12-11 02:06:20 -0300

    Alteração de class na 'home'

--- src/paginas/AppHome.js
+++ src/paginas/AppHome.js
@@ -89,7 +89,7 @@ function AppHome() {
             <h3>Rua Conde de Bonfim, RJ</h3>
             <h4>4 Quartos | 3 Banheiros | 2 Garagens</h4>
             <h4>Falar com Orlando: (21) 99040-6500</h4>
-            <img class="image" src="https://imgbr.imovelwebcdn.com/avisos/2/29/72/81/99/28/720x532/3588585487.jpg" alt="money" />
+            <img class="imagem-card" src="https://imgbr.imovelwebcdn.com/avisos/2/29/72/81/99/28/720x532/3588585487.jpg" alt="money" />
           </div>
 
           <div class="card green">
@@ -97,7 +97,7 @@ function AppHome() {
             <h3>Rua Oswaldo Lussac, RJ</h3>
             <h4>2 Quartos | 3 Banheiros | 1 Garagem</h4>
             <h4>Falar com Mariana: (21) 97980-1221</h4>
-            <img class="image" src="https://system.soprojetos.com.br/files/1335/medium/projeto-de-casa-terrea-para-terrenos-de-10-metros-com-3-quartos-e-1-suite-cod-175-fachada-1.jpg?1576186538" alt="money" />
+            <img class="imagem-card" src="https://system.soprojetos.com.br/files/1335/medium/projeto-de-casa-terrea-para-terrenos-de-10-metros-com-3-quartos-e-1-suite-cod-175-fachada-1.jpg?1576186538" alt="money" />
           </div>
 
           <div class="card green">
@@ -105,7 +105,7 @@ function AppHome() {
             <h3>Almirante Ary Parreiras, RJ</h3>
             <h4>1 Quarto | 1 Banheiro | 1 Garagem</h4>
             <h4>Falar com Lara: (21) 98356-6482</h4>
-            <img class="image" src="https://system.soprojetos.com.br/files/996/medium/modelo_de_casa__versao_do_cod_99-cod_136-foto-1.jpg?1491503841" alt="money_1" />
+            <img class="imagem-card" src="https://system.soprojetos.com.br/files/996/medium/modelo_de_casa__versao_do_cod_99-cod_136-foto-1.jpg?1491503841" alt="money_1" />
           </div>
 
           <div class="card green">
@@ -114,7 +114,7 @@ function AppHome() {
             <h3>Avenida Atlântica, RJ</h3>
             <h4>3 Quartos | 2 Banheiros | 1 Garagem</h4>
             <h4>Falar com Yasmin: (21) 97205-4101</h4>
-            <img class="image" src="https://system.soprojetos.com.br/files/1491/medium/PAD-COD43-FOTO-1-WEB.jpg?1659015535" alt="money" />
+            <img class="imagem-card" src="https://system.soprojetos.com.br/files/1491/medium/PAD-COD43-FOTO-1-WEB.jpg?1659015535" alt="money" />
             <br></br>
           </div>
 
@@ -123,7 +123,7 @@ function AppHome() {
             <h3>Rua Porto Amazonas, RJ</h3>
             <h4>3 Salas | 2 Banheiros | 2 Garagens</h4>
             <h4>Falar com Brian: (21) 99255-0986</h4>
-            <img class="image" src="https://system.soprojetos.com.br/files/615/medium/casa_popular_cod100_2quartos.jpg?1491510183" alt="money" />
+            <img class="imagem-card" src="https://system.soprojetos.com.br/files/615/medium/casa_popular_cod100_2quartos.jpg?1491510183" alt="money" />
           </div>
 
           <div class="card green">
@@ -132,7 +132,7 @@ function AppHome() {
             <h3>Rua Voluntários da Pátria, RJ</h3>
             <h4>2 Salas | 2 Banheiros | 0 Garagem</h4>
             <h4>Falar com Gabrielly: (21) 99483-5925</h4>
-            <img class="image" src="https://system.soprojetos.com.br/files/597/medium/primeira_obra_da_madrugada.jpg?1491504043" alt="money" />
+            <img class="imagem-card" src="https://system.soprojetos.com.br/files/597/medium/primeira_obra_da_madrugada.jpg?1491504043" alt="money" />
           </div>
         </div>
       </div>
